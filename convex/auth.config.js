e4296477701745f325ad7c5933fa266fffab7d07
Jambr0li <jasonkovalenko@gmail.com>
2025-10-12 21:12:55 -0500

    wip

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://elegant-finch-58.clerk.accounts.dev",
+        domain: process.env.CLERK_FRONTEND_API_URL,
         applicationID: 'convex',
       },
     ],
